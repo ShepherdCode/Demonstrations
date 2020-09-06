6bc0551a73f014991f65930d0cfefc0fbbd4fefd
JasonRafeMiller <jmill02@shepherd.edu>
2020-09-06 06:43:53 -0400

    Support negative exponent.

--- EfficientPowerFunction/EfficientPowerFunction.java
+++ EfficientPowerFunction/EfficientPowerFunction.java
@@ -14,7 +14,9 @@ public class EfficientPowerFunction {
     public double power (double base, int exp){
         iterations++;
         double answer;
-        if (exp <= 0) {   
+        if (exp < 0) {
+            return 1.0/power(base,0-exp);
+        } else if (exp == 0) {   
             return 1;
         } else if (exp == 1) {
             return base;
@@ -36,19 +38,31 @@ public class EfficientPowerFunction {
         int calls1,calls2;
         int base = 2;
         int exp;
-        System.out.println("          calls calls"); 
-        System.out.println("base expo NoOpt Optimized"); 
-        System.out.println("==== ==== ===== ========="); 
-        for (exp=1; exp<=20; exp++) {
-            math1 = new EfficientPowerFunction(false);
-            answer1 = math1.power(base,exp);
-            calls1 = math1.getIterations();
-            math2 = new EfficientPowerFunction(true);
-            answer2 = math2.power(base,exp);
-            calls2 = math2.getIterations();
-            System.out.printf(
-            "%4d %4d %5d %5d\n",
-            base, exp, calls1, calls2); 
+        boolean testOptimize = true;
+        if (testOptimize) {
+            System.out.println("          calls calls"); 
+            System.out.println("base expo NoOpt Optimized"); 
+            System.out.println("==== ==== ===== ========="); 
+            for (exp=1; exp<=20; exp++) {
+                math1 = new EfficientPowerFunction(false);
+                answer1 = math1.power(base,exp);
+                calls1 = math1.getIterations();
+                math2 = new EfficientPowerFunction(true);
+                answer2 = math2.power(base,exp);
+                calls2 = math2.getIterations();
+                System.out.printf(
+                    "%4d %4d %5d %5d\n",
+                    base, exp, calls1, calls2); 
+            }
+        }
+        boolean testNegative = true;
+        if (testNegative) {
+            System.out.println();
+            math1 = new EfficientPowerFunction(true);
+            answer1 = math1.power(10,3);
+            System.out.println("power(10,3)="+answer1);
+            answer1 = math1.power(10,-3);
+            System.out.println("power(10,-3)="+answer1);
         }
     }
 }
