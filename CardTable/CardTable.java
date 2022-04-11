b0c58457be0b07f713ce2f9e589a8f362e71c438
JasonRafeMiller <jmill02@shepherd.edu>
2022-04-11 17:43:58 -0400

    start CardTable just for UML

---
+++ CardTable/CardTable.java
@@ -0,0 +1,39 @@
+
+/**
+ * Write a description of class CardTable here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class CardTable
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class CardTable
+     */
+    public CardTable()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    public void draw (CardCollection cards, int type) {
+        // type 0 = deck
+        // type 1 = hand 1
+        // type 2 = hand 2
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
